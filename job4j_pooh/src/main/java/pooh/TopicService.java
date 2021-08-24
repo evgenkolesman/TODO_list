072df1ce52affbd91adeb4e9f7f5b5675df14156
evgenkolesman <glavstroi_e@mail.ru>
2021-08-24 12:15:09 +0300

    Initial commit

---
+++ job4j_pooh/src/main/java/pooh/TopicService.java
@@ -0,0 +1,13 @@
+package pooh;
+
+import java.security.Provider;
+import java.util.List;
+import java.util.Map;
+
+public class TopicService implements Service {
+
+    @Override
+    public Resp process(Req req) {
+        return null;
+    }
+}
