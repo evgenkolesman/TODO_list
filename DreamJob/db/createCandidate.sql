51deb1c386454631961f1f00e530dc0ff66c8ad3
Evgeniy Kolesnikov <70563563+evgenkolesman@users.noreply.github.com>
2021-09-09 16:40:05 +0300

    Delete DreamJob directory

--- DreamJob/db/createCandidate.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE IF NOT EXISTS candidate (
-                                         id SERIAL PRIMARY KEY ,
-                                         name TEXT
-);
--- DreamJob/db/createUsers.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-                                     id SERIAL PRIMARY KEY,
-                                     name TEXT,
-                                     email TEXT UNIQUE,
-                                     password TEXT
-);
